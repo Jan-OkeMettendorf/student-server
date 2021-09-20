0394445b5133fa6bfcf6b342e094b67398673e73
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-20 14:07:23 +0200

    Add project

---
+++ src/test/java/de/neuefische/springserver/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
